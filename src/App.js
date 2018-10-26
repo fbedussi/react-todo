597f4b9b61667fb8d96695e7354dc91f06f5774a
Francesco Bedussi <francesco.bedussi@ynap.com>
2018-10-26 13:46:16 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos: []
+    };
+  }
+
   render() {
+    const {todos} = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={(ev) => {
+            ev.preventDefault();
+            const newTodo = ev.target[0].value;
+            if (newTodo.length) {
+              this.setState({todos: todos.concat(newTodo)});
+            }
+          }}>
+          <input type="text" placeholder="meet granpa"/>
+          <button type="submit">save</button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+          <li key={index}>            
+            {todo}
+            <button type="button" onClick={() => {
+              this.setState({todos: todos.filter((todo, i) => i !== index)})
+            }}>x</button>
+          </li>))}
+        </ul>
       </div>
     );
   }
