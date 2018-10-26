c150e1cb6c8e4bf5b5920df1cc7e072966100549
Francesco Bedussi <francesco.bedussi@ynap.com>
2018-10-26 13:49:38 +0200

    extract methods

--- src/App.js
+++ src/App.js
@@ -8,31 +8,42 @@ class App extends Component {
     this.state = {
       todos: []
     };
+
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  addTodo(ev) {
+    ev.preventDefault();
+    const { todos } = this.state;
+    const newTodo = ev.target[0].value;
+
+    if (newTodo.length) {
+      this.setState({ todos: todos.concat(newTodo) });
+      ev.target[0].value = '';
+    }
+  }
+
+  deleteTodo(index) {
+    const { todos } = this.state;
+
+    this.setState({ todos: todos.filter((todo, i) => i !== index) });
   }
 
   render() {
-    const {todos} = this.state;
+    const { todos } = this.state;
 
     return (
       <div className="App">
-        <form onSubmit={(ev) => {
-            ev.preventDefault();
-            const newTodo = ev.target[0].value;
-            if (newTodo.length) {
-              this.setState({todos: todos.concat(newTodo)});
-            }
-          }}>
-          <input type="text" placeholder="meet granpa"/>
+        <form onSubmit={this.addTodo}>
+          <input type="text" placeholder="meet granpa" />
           <button type="submit">save</button>
         </form>
         <ul>
           {todos.map((todo, index) => (
-          <li key={index}>            
-            {todo}
-            <button type="button" onClick={() => {
-              this.setState({todos: todos.filter((todo, i) => i !== index)})
-            }}>x</button>
-          </li>))}
+            <li key={index}>
+              {todo}
+              <button type="button" onClick={() => this.deleteTodo(index)}>x</button>
+            </li>))}
         </ul>
       </div>
     );
